3fe98c07129d26d432f0c65bd52d14fd83dacd59
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2018-12-03 22:12:42 -0500

    adds faces

--- two-js/flower/scripts/pattern.js
+++ two-js/flower/scripts/pattern.js
@@ -1,26 +1,53 @@
 var elem = document.getElementById('draw-pattern');
 var params = {
-  width: 300,
-  height: 300
+  width: 500,
+  height: 500
 };
 var two = new Two(params).appendTo(elem);
-elem.style.backgroundColor = 'green';
+elem.style.backgroundColor = 'tan';
 
-x = 25;
-y = 25;
+x = 50;
+y = 50;
 
-let mainPattern = two.makeGroup();
-let decoration = two.makeGroup();
+let head = two.makeGroup();
+let leftEye = two.makeGroup();
+let leftPupil = two.makeGroup();
+let rightEye = two.makeGroup();
+let rightPupil = two.makeGroup();
+let nose = two.makeGroup();
+let mouth = two.makeGroup();
+let leftEyebrow = two.makeGroup();
+let rightEyebrow = two.makeGroup();
+
 
 for (i = 0; i < 15; i++) {
-
   for (j = 0; j < 15; j++) {
-    for (k = 4; k > 0; k--) {
-      decoration.add(two.makeCircle(x * i, y * j, k * k));
+    for (k = 1; k > 0; k--) {
+      head.add(two.makeCircle(x * i, y * j, k * 22));
+      leftEye.add(two.makeCircle(x * i - 10, y * j, k * 4));
+      leftPupil.add(two.makeCircle(x * i - 10, y * j, k * 2));
+      rightEye.add(two.makeCircle(x * i + 10, y * j, k * 4));
+      rightPupil.add(two.makeCircle(x * i + 10, y * j, k * 2));
+      nose.add(two.makePolygon(x * i, y * j + 7, k * 2));
+      mouth.add(two.makeCurve(x * i - 10, y * j + 10,
+        x * i - 5, y * j + 15,
+        x * i, y * j + 14,
+        x * i + 5, y * j + 15,
+        x * i + 10, y * j + 10,
+        true));
+      leftEyebrow.add(two.makeCurve(x * i - 15, y * j - 8,
+        x * i - 10, y * j - 10,
+        x * i - 5, y * j - 8, true));
+      rightEyebrow.add(two.makeCurve(x * i + 15, y * j - 8,
+        x * i + 10, y * j - 10,
+        x * i + 5, y * j - 8, true));
     }
   }
 }
 
+
+leftPupil.fill = 'black';
+rightPupil.fill = 'black';
 
 
 
