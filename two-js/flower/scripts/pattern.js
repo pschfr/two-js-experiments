7440d8b9ec7bc0e4fb4c61cc6164e275382dc2da
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2018-12-03 21:41:07 -0500

    adds pattern #4

--- two-js/flower/scripts/pattern.js
+++ two-js/flower/scripts/pattern.js
@@ -4,39 +4,24 @@ var params = {
   height: 300
 };
 var two = new Two(params).appendTo(elem);
-elem.style.backgroundColor = 'pink';
+elem.style.backgroundColor = 'green';
 
 x = 25;
 y = 25;
 
 let mainPattern = two.makeGroup();
-let mainPattern2 = two.makeGroup();
-let mainPattern3 = two.makeGroup();
-let backgroundPattern = two.makeGroup();
 let decoration = two.makeGroup();
 
 for (i = 0; i < 15; i++) {
 
   for (j = 0; j < 15; j++) {
-    for (k = 3; k > 0; k--) {
+    for (k = 4; k > 0; k--) {
       decoration.add(two.makeCircle(x * i, y * j, k * k));
-      mainPattern.add(two.makeCircle(x * i, y * j, 10));
-      mainPattern2.add(two.makeCircle(x * i + 2, y * j, 10));
-      mainPattern3.add(two.makeCircle(x * i + 4, y * j, 10));
-      //backgroundPattern.add(two.makePolygon(x * i + 25 / 2, y * j + 25 / 2, 15));
     }
   }
 }
 
-mainPattern.fill = 'yellow';
-mainPattern.stroke = 'none';
-mainPattern2.fill='red';
-mainPattern2.opacity = '.5';
-mainPattern2.stroke = 'none';
-mainPattern3.opacity = '.3';
-mainPattern3.stroke = 'none';
-mainPattern3.fill = 'white';
-decoration.stroke = 'red';
+
 
 
 two.update();
