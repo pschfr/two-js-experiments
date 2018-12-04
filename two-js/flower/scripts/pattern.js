569b42a3c00b6fa672cf1b05126191284a26854a
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2018-12-03 21:07:07 -0500

    adds pattern #2

--- two-js/flower/scripts/pattern.js
+++ two-js/flower/scripts/pattern.js
@@ -4,32 +4,32 @@ var params = {
   height: 300
 };
 var two = new Two(params).appendTo(elem);
-elem.style.backgroundColor = 'red';
+elem.style.backgroundColor = '#fd6a02';
 
 x = 25;
-y = 26;
+y = 25;
 
 let mainPattern = two.makeGroup();
 let mainPattern2 = two.makeGroup();
 let backgroundPattern = two.makeGroup();
 let decoration = two.makeGroup();
 
-for (i = 0; i < 20; i++) {
+for (i = 0; i < 15; i++) {
 
   for (j = 0; j < 15; j++) {
-    for (k = 3; k > 0; k--) {
-      decoration.add(two.makeCircle(x * i, y * j, k * k));
-      mainPattern.add(two.makeCircle(x * i, y * j, k * Math.PI));
-      mainPattern2.add(two.makeRectangle(x * i, y * j, 20, 20));
-      backgroundPattern.add(two.makeRectangle(x * i + 25 / 2, y * j + 25 / 2, 5, 5));
+    for (k = 15; k > 0; k--) {
+      // decoration.add(two.makeCircle(x * i, y * j, j));
+      mainPattern.add(two.makeCircle(x * i, y * j, k * j/Math.PI));
+      // mainPattern2.add(two.makeStar(x * i, y * j, 20, 20));
+      // backgroundPattern.add(two.makeRectangle(x * i + 25 / 2, y * j + 25 / 2, 5, 5));
     }
   }
 }
 
 mainPattern2.fill = 'none';
-mainPattern2.stroke = 'blue';
-mainPattern.fill = 'yellow';
-mainPattern.stroke = 'olive';
+mainPattern2.stroke = 'orange';
+mainPattern.fill = 'white';
+mainPattern.stroke = 'blue';
 mainPattern2.linewidth = '2';
 backgroundPattern.stroke = 'darkgreen';
 backgroundPattern.fill = 'pink';
